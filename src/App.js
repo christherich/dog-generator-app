3ac308fe914cb33803bbd4ae8e60f0d421fb1f3a
richthechris <chrisafonso03@gmail.com>
2023-04-03 08:57:55 -0300

    version1.0

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [imageUrl, setImageUrl] = useState("");
+
+  const getDogImage = () => {
+    axios
+      .get("https://dog.ceo/api/breeds/image/random")
+      .then((response) => setImageUrl(response.data.message))
+      .catch((error) => console.log(error));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>random dog generator lol</h1>
+      <button onClick={getDogImage}>woof! woof!</button>
+      <br />
+      {imageUrl && <img src={imageUrl} alt="Dog" />}
     </div>
   );
 }
